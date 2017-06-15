4ee4bb5f75370bebea54ec252d3f2b3b825974b9
孙立阳 <1204007548@qq.com>
2017-06-15 15:12:26 +0800

    first commit

---
+++ sly_middle_config/src/main/java/com/sly/AbstractInterceptor.java
@@ -0,0 +1,59 @@
+package com.sly;
+import java.util.ArrayList;
+import java.util.List;
+
+import org.apache.ibatis.mapping.BoundSql;
+import org.apache.ibatis.mapping.MappedStatement;
+import org.apache.ibatis.mapping.MappedStatement.Builder;
+import org.apache.ibatis.mapping.ResultMap;
+import org.apache.ibatis.mapping.ResultMapping;
+import org.apache.ibatis.mapping.SqlSource;
+import org.apache.ibatis.plugin.Interceptor;
+
+/**
+ * 
+ * <b>Description：</b> Mybatis拦截器基类 <br/>
+ * <b>ClassName：</b> AbstractInterceptor <br/>
+ * <b>@author：</b> jackyshang <br/>
+ * <b>@date：</b> 2016年7月11日 下午6:51:48 <br/>
+ * <b>@version: </b>  <br/>
+ */
+public abstract class AbstractInterceptor implements Interceptor {
+	protected MappedStatement copyFromMappedStatement(MappedStatement ms, SqlSource newSqlSource, boolean isCount) {
+		Builder builder = new MappedStatement.Builder(ms.getConfiguration(), ms.getId(), newSqlSource, ms.getSqlCommandType());
+		builder.resource(ms.getResource());
+		builder.fetchSize(ms.getFetchSize());
+		builder.statementType(ms.getStatementType());
+		builder.keyGenerator(ms.getKeyGenerator());
+		String[] s = ms.getKeyProperties();
+		if (s == null) {
+			builder.keyProperty(null);
+		} else {
+			builder.keyProperty(s[0]);
+		}
+		builder.timeout(ms.getTimeout());
+		builder.parameterMap(ms.getParameterMap());
+		if (isCount) {
+			List<ResultMap> resultMaps = new ArrayList<ResultMap>();
+			resultMaps.add(new ResultMap.Builder(ms.getConfiguration(), ms.getId(), Integer.class, new ArrayList<ResultMapping>()).build());
+			builder.resultMaps(resultMaps);
+		} else {
+			builder.resultMaps(ms.getResultMaps());
+		}
+		builder.cache(ms.getCache());
+		MappedStatement newMs = builder.build();
+		return newMs;
+	}
+
+	public static class BoundSqlSqlSource implements SqlSource {
+		BoundSql boundSql;
+
+		public BoundSqlSqlSource(BoundSql boundSql) {
+			this.boundSql = boundSql;
+		}
+
+		public BoundSql getBoundSql(Object parameterObject) {
+			return boundSql;
+		}
+	}
+}
